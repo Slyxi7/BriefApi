732c380d240fb9543f387abf775c0b93169ba856
Alexandre Crestien <alexandre.crestien@email.com>
2026-02-24 14:42:00 +0100

    feat: add password in schema user

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,3 +1,4 @@
+import re
 from pydantic import BaseModel, EmailStr, field_validator
 from datetime import datetime
 
@@ -8,20 +9,46 @@ class UserBase(BaseModel):
     prenom: str
     email: EmailStr
     role: str
-
+   
     @field_validator("role")
-    def validate_role(cls, i):
-        if i not in ROLES:
+    def validate_role(cls, val):
+        if val not in ROLES:
             raise ValueError(f"Rôle invalide. Choisir parmi : {ROLES}")
-        return i
+        return val
 
 class UserCreate(UserBase):
-    pass
+    password: str
+
+    @field_validator("password")
+    def validate_password(cls, val):
+        if len(val) < 8:
+            raise ValueError("Le mot de passe doit contenir au moins 8 caractères")
+        
+        if not re.search(r"[A-Z]", val):
+            raise ValueError("Le mot de passe doit contenir au moins une majuscule")
+
+        if not re.search(r"[a-z]", val):
+            raise ValueError("Le mot de passe doit contenir au moins une minuscule")
+        
+        if not re.search(r"[0-9]", val):
+            raise ValueError("Le mot de passe doit contenir au moins un chiffre")
+
+        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", val):
+            raise ValueError("Le mot de passe doit contenir au moins un symbole")
+        return val
+
 
 class UserUpdate(BaseModel):
     nom: str | None = None
     prenom: str | None = None
+    email: EmailStr | None = None
     role: str | None = None
+
+    @field_validator("role")
+    def validate_role(cls, val):
+        if val not in ROLES:
+            raise ValueError(f"Rôle invalide. Choisir parmi : {ROLES}")
+        return val
 
 class UserRead(UserBase):
     id: int
