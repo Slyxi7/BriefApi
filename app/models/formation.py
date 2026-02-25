082a26be71170227bf4d481ed9d9e5c0188468ec
Alexandre Crestien <alexandre.crestien@email.com>
2026-02-25 15:59:25 +0100

    feat: add enum in models

--- app/models/formation.py
+++ app/models/formation.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import Text, Integer, String
+from sqlalchemy import Enum as SQLEnum
 from base import Base
+from enums.level import Level
 
 class Formation(Base):
     __tablename__ = "formations"
@@ -9,4 +11,4 @@ class Formation(Base):
     titre : Mapped[str] = mapped_column(String(100), nullable=False)
     description : Mapped[str] = mapped_column(Text)
     duree : Mapped[int] = mapped_column(Integer, nullable=False)
-    niveau : Mapped[str] = mapped_column(String(50), nullable=False)
+    niveau: Mapped[Level] = mapped_column(SQLEnum(Level), nullable=False)
--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,9 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import DateTime, String
+from sqlalchemy import Enum as SQLEnum
 from datetime import datetime
 from base import Base
-
+from enums.roles import Roles
 class User(Base):
     __tablename__ = "users"
 
@@ -10,6 +11,6 @@ class User(Base):
     nom: Mapped[str] = mapped_column(String(50), nullable=False)
     prenom: Mapped[str] = mapped_column(String(50), nullable=False)
     email: Mapped[str] = mapped_column(String(100), nullable=False)
-    role: Mapped[str] = mapped_column(String(50), nullable=False)
+    role: Mapped[Roles] =  mapped_column(SQLEnum(Roles), nullable=False)
     date_inscription: Mapped[datetime] = mapped_column(DateTime, nullable=False)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
