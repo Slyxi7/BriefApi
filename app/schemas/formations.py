4c988582ecca2bc37549c1c2de6c9701b9470ae2
Alexandre Crestien <alexandre.crestien@email.com>
2026-02-26 09:44:16 +0100

    refactor: remove Base schemas and correct Read model

--- app/schemas/formations.py
+++ app/schemas/formations.py
@@ -1,18 +1,12 @@
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, field_validator, ConfigDict
 from typing import Optional
 from enums.level import Level
 
-class FormationBase(BaseModel):
+class FormationCreate(BaseModel):
     titre: str
     description: str | None = None
     duree: int
     niveau: Level
-   
-    @field_validator("niveau")
-    def validate_niveau(cls, val):
-        if val not in Level:
-            raise ValueError(f"Niveau invalide. Choisir parmi : {Level}")
-        return val
 
     @field_validator("duree")
     def validate_duree(cls, val):
@@ -20,31 +14,28 @@ class FormationBase(BaseModel):
             raise ValueError("La durée doit être > 0")
         return val
 
-class FormationCreate(FormationBase):
-    pass
-
-class FormationUpdate(FormationBase):
+class FormationUpdate(BaseModel):
     titre: Optional[str] = None
     description: Optional[str] = None
     duree: Optional[int] = None
     niveau: Optional[Level] = None
-   
-    @field_validator("niveau")
-    def validate_niveau(cls, val):   
-        if val not in Level:
-            raise ValueError(f"Niveau invalide. Choisir parmi : {Level}")
-        return val
 
     @field_validator("duree")
     def validate_duree(cls, val):
+        if val is None:
+            return val
         if val <= 0:
             raise ValueError("La durée doit être > 0")
         return val
 
-class InscriptionRead(FormationBase):
+class FormationRead(BaseModel):
     id: int
+    titre: str
+    description: str | None = None
+    duree: int
+    niveau: Level
 
-    class Config: from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
 
-class InscriptionDelete(BaseModel):
+class FormationDelete(BaseModel):
     hard: bool = False
