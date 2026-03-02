cc49e11e1a48f6cfc6683d07c1b0cb6a37395b5f
Alexandre Crestien <alexandre.crestien@email.com>
2026-03-02 14:15:49 +0100

    fix: clean gestion error inscription

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from app.routers import user_router, formation_router, session_router
+from app.routers import user_router, formation_router, session_router, inscription_router
 
 app = FastAPI(title="Simplon Training API")
 
 app.include_router(user_router.router)
 app.include_router(formation_router.router)
 app.include_router(session_router.router)
-##app.include_router(inscription_router.router)
+app.include_router(inscription_router.router)
